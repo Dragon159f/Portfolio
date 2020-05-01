10e6223d7b7eb505c69c82f5a3fedb3d1a578b6f
Jason <jasongiang159@gmail.com>
2020-05-01 10:52:29 -0700

    added pictures and made ui look a little bit cleaner

--- js/script.js
+++ js/script.js
@@ -33,3 +33,29 @@
 
 
 // Add your click handler below to update your like counters when the like buttons are clicked. 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-red", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-red";
+}
